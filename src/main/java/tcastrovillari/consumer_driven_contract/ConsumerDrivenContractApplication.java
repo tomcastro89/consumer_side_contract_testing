b8faea7b1db39045385df077908eaab174ff296b
tommaso <tcastrovillari@itemis.de>
2019-11-08 11:18:48 +0100

    changed to consumer

--- src/main/java/tcastrovillari/consumer_driven_contract/ConsumerDrivenContractApplication.java
+++ src/main/java/tcastrovillari/consumer_driven_contract/ConsumerDrivenContractApplication.java
@@ -2,12 +2,16 @@ package tcastrovillari.consumer_driven_contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumerDrivenContractApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ConsumerDrivenContractApplication.class, args);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject("http://localhost:8081/endpoint", Object.class);
     }
 
 }
