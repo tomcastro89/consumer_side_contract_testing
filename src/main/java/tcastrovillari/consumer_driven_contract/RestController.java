e444842df81bb619bebcfb280761fd1c0630ad95
tommaso <tcastrovillari@itemis.de>
2019-11-08 11:10:06 +0100

    refactored

--- src/main/java/tcastrovillari/consumer_driven_contract/RestController.java
+++ src/main/java/tcastrovillari/consumer_driven_contract/RestController.java
@@ -2,10 +2,6 @@ package tcastrovillari.consumer_driven_contract;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
